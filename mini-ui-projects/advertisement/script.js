6f08dc9e97a7a1e59fdc432637bf1ac967905ea9
kaimikan <oihic@protonmail.com>
2024-10-02 22:15:18 +0300

    Advertisement

---
+++ mini-ui-projects/advertisement/script.js
@@ -0,0 +1,7 @@
+function showMessage() {
+  document.getElementById('popup-message').style.display = 'block';
+}
+
+function closeMessage() {
+  document.getElementById('popup-message').style.display = 'none';
+}
